b164cb7e109d83f007dde1ddd693feb288931977
Pavel Smirnov <tratata@mail.ru>
2020-03-14 13:39:04 +0300

    Changen main.c

--- project/src/main.c
+++ project/src/main.c
@@ -2,17 +2,9 @@
 #include "array.h"
 
 #include <stdio.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <assert.h>
-
-#include <math.h>
-
-#include "time.h"
-
 #include <omp.h>
+
+#define N_ITERATIONS 5
 
 static __inline__ unsigned long long rdtsc(void)
 {
@@ -20,7 +12,6 @@ static __inline__ unsigned long long rdtsc(void)
   __asm__ __volatile__ ("rdtsc" : "=a"(lo), "=d"(hi));
   return ( (unsigned long long)lo)|( ((unsigned long long)hi)<<32 );
 }
-
 
 
 int main(int argc, char* argv[]) {
@@ -32,36 +23,29 @@ int main(int argc, char* argv[]) {
   }
 
   matrix *mat_ptr = create_matrix_from_file(argv[1]);
-  //print_matrix(mat_ptr);
-  printf("\n");
+  if (mat_ptr == NULL) {
+    printf("Invalid matrix file\n");
+    return 0;
+  }
 
-
+  long cycles = 0;
+  double wtime = 0.0;
 
   double wstart = omp_get_wtime();
   long cycles1 = rdtsc();
-  unsigned int start = clock();
-  array *arr_ptr1 = matrix_col_sum(mat_ptr);
-  unsigned int finish = clock();
+  array *arr_ptr = matrix_col_sum(mat_ptr);
   long cycles2 = rdtsc();
   double wfinish = omp_get_wtime();
 
-  printf("long method \nproc time: %d\ntacts: %ld\nwtime: %lf\n\n", finish - start, cycles2 - cycles1, wfinish - wstart);
+  wtime = wfinish - wstart;
+  cycles = cycles2 - cycles1;
+
+  free_array(arr_ptr);
+
+  printf("Matrix %zux%zu\n", mat_ptr->rows, mat_ptr->cols);
+  printf("proc cycles: %ld\nwtime: %lf\n\n", cycles, wtime);
 
 
-
-
- /* for (size_t i = 0; i < arr_ptr1->size; i++) {
-      if (fabs(arr_ptr1->data[i] - arr_ptr2->data[i]) > 0.00001) {
-        printf("%lf  diff12\n", arr_ptr1->data[i] - arr_ptr2->data[i]);
-        break;
-      }
-    if (fabs(arr_ptr1->data[i] - arr_ptr3->data[i]) > 0.00001) {
-      printf("%lf  diff13\n", arr_ptr1->data[i] - arr_ptr3->data[i]);
-      break;
-    }
-  } */
-
-  free_array(arr_ptr1);
   free_matrix(mat_ptr);
   return 0;
 }
